aaf9349b964036b3835655fb92648e3cfee3df94
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2024-03-16 22:11:42 +0800

    Added table task record, Added seeder

---
+++ php/src/application/migrations/003_create_task_record_table.php
@@ -0,0 +1,67 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Create_Task_Record_Table extends CI_Migration
+{
+    private $table='task_record';
+
+    public function up()
+    {
+        $this->dbforge->add_field(array(
+                        'id' => array(
+                                'type' => 'INT',
+                                'unsigned' => TRUE,
+                                'auto_increment' => TRUE,
+                        ),
+                        'task_id' => array(
+                                'type' => 'INT',
+                                'unsigned' => TRUE,
+                        ),
+                        'message' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => '100',
+                        ),
+                        'image_url' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => '100',
+                        ),
+                ));
+        $this->dbforge->add_key('id',true);
+        $this->dbforge->create_table($this->table);
+
+        # Seeding user table
+        $data = array(
+                array(
+                        'id' => 1,
+                        'task_id' => 1,
+                        'message' => 'Nearly done',
+                        'image_url' => '78633642_2575512575897642_4055321345087504384_n.jpg',
+                ),
+                array(
+                        'id' => 2,
+                        'task_id' => 1,
+                        'message' => 'Starting to work in this',
+                        'image_url' => '78633642_2575512575897642_4055321345087504384_n.jpg',
+                ),
+                array(
+                        'id' => 3,
+                        'task_id' => 1,
+                        'message' => 'I have troubled in this',
+                        'image_url' => '78633642_2575512575897642_4055321345087504384_n.jpg',
+                ),
+                array(
+                        'id' => 4,
+                        'task_id' => 2,
+                        'message' => 'Is this okay?',
+                        'image_url' => '78633642_2575512575897642_4055321345087504384_n.jpg',
+                )
+        );
+        $this->db->insert_batch($this->table, $data);
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_table($this->table);
+    }
+}
